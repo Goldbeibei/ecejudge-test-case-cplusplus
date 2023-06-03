eaa492001eff2f15d9b2ea6797bd981209e2530b
Goldbeibei <b0942103@ems.niu.edu.tw>
2023-06-04 02:53:32 +0800

    更新判斷路徑是否存在
    判斷路徑輸入路徑是否存在，若不存在則繼續要求輸入

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ import os
 # import re
 import subprocess
 
-# 輸入測資資料夾編號
-question_number = input("請輸入測資資料夾編號：")
-
 # 輸入C++的編譯後程式名稱(目前失敗)
 # my_cpp_code_exe = "'./"+input("C++的編譯後程式名稱：")+"'"
 # print(my_cpp_code_exe)
 
-# 設置 .in 和 .out 文件夾的路徑
-input_dir = './problem_'+question_number+'_test_cases'
-output_dir = './problem_'+question_number+'_test_cases'
+# 判斷路徑輸入路徑是否存在，若不存在則繼續要求輸入
+while True:
+    try:
+        # 輸入測資資料夾編號
+        question_number = input("請輸入測資資料夾編號：")
+        # 設置 .in 和 .out 文件夾的路徑
+        input_dir = './problem_'+question_number+'_test_cases'
+        output_dir = './problem_'+question_number+'_test_cases'
+        # 檢查路徑是否存在
+        if not os.path.exists(input_dir):
+            raise FileNotFoundError
+        # 如果路徑存在，就中斷迴圈
+        break
+    except FileNotFoundError:
+        print("指定的路徑不存在，請檢查路徑是否正確")
+
 
 # 獲取 .in 文件夾中的所有文件名
 input_files = os.listdir(input_dir)
