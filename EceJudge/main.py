241c1a12ea80e9d04395434685e50e30dae4eb71
Goldbeibei <102613276+Goldbeibei@users.noreply.github.com>
2023-06-26 05:42:13 +0800

    新增ecejudge登入並進入到新增題目區

---
+++ EceJudge/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 04:29:13 2023
+
+@author: JF
+"""
+
+# 登入ecejudge.tk的網址端120.101.8.132
+!pytest -v login.py
+
+# 登出ecejudge.tk的網址端120.101.8.132(目前失敗)
+#!pytest -v logout.py
