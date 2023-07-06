346f2a10a3c7f9d8ac7dbd38d27e22d5e35f1e80
Goldbeibei <b0942103@ems.niu.edu.tw>
2023-07-06 13:59:49 +0800

    UPDATE

--- ans.cpp
+++ ans.cpp
@@ -1,20 +1,18 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main() {
-    int N;
-    while (cin >> N) {
-        int sum = 0;
-        for (int i = 1; i < N; i++) {
-            if (N % i == 0)
-                sum += i;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((a&&b)==c){
+        cout<<"And"<<endl;
         }
-        if (sum > N)
-            cout << "盈數\n";
-        else if (sum == N)
-            cout << "完全數\n";
-        else
-            cout << "虧數\n";
+    if(a||b==c){
+        cout<<"Or"<<endl;
     }
-    return 0;
+    if(a^b==c){
+        cout<<"Xor"<<endl;
+    }
+    if((a&&b)!=c and (a||b!=c) and (a^b!=c)){
+        cout<<"Impossible"<<endl;
+    }
 }
